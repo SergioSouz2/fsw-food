93caa1cc22b6034c8939a96700767226928434d6
SergioSouz2 <sergio.tbl0123@gmail.com>
2025-01-08 16:56:31 -0300

    :sparkles:	feat: add initial search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,20 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const Search = () => {
+   return (
+      <div className="flex gap-2">
+         <Input placeholder="Buscar Restaurantes" className="border-none" />
+         <Button
+            size={"icon"}
+            variant={"default"}
+            className="border-none"
+         >
+            <SearchIcon size={20} />
+         </Button>
+      </div>
+   );
+}
+
+export default Search;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
    return (
-      <div className=" ">
+      <div className="px-5">
          <Header />
+         <div className="pt-6">
+            <Search />
+         </div>
       </div >
    );
 }
