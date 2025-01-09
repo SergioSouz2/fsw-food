7f06d75916b62ad0caf9a5936d7a2c594ba63bae
SergioSouz2 <sergio.tbl0123@gmail.com>
2025-01-09 11:22:59 -0300

    :sparkles:	feat: add banner promotional

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -12,6 +13,30 @@ export default function Home() {
 
          <div className="pt-6">
             <CategoryList />
+         </div>
+
+         <div className="pt-6">
+            <Image
+               src={'/banner_pizza.png'}
+               alt="Até 30% de desconto em pizzas!"
+               width={0}
+               height={0}
+               className="w-full h-auto object-contain"
+               sizes="100vw"
+               quality={100}
+            />
+         </div>
+
+         <div className="pt-6">
+            <Image
+               src={'/banner_burguer.png'}
+               alt="A partir de R$17,00 em lanches"
+               width={0}
+               height={0}
+               className="w-full h-auto object-contain"
+               sizes="100vw"
+               quality={100}
+            />
          </div>
       </div >
    );
